7ab681a59b8370bce6c10fbaaa59f4228e074ee7
Jose Encinoza <joseEncinoza07@gmail.com>
2018-05-04 21:51:52 -0400

    Base proyecto

---
+++ controllers/imagen.js
@@ -0,0 +1,111 @@
+//----dependencias------  
+'use strict'
+const bcrypt = require("bcryptjs");
+const Imagen = require('../models/imagen');
+
+exports.findDocuments = (req,res) => {
+  
+  Imagen.forge().fetchAll()
+  .then(function(data){
+    res.status(200).json({ error : false, data : data.toJSON() });
+  })
+  .catch(function (err) {
+    res.status(500).json({ error: true, data: {message: err.message} });
+  });
+
+}
+
+exports.createDocument = (req,res) => {
+
+  let newData = {
+    id_sistema:         req.body.id_sistema,
+    imagen:             req.body.imagen,
+    titulo:             req.body.cedula,
+    descripcion:        req.body.descripcion,
+    fecha_creacion:     req.body.fecha_creacion,
+    estatus:            req.body.estatus,
+    tipo_imagen:        req.body.tipo_imagen,
+  }
+
+  Imagen.forge(newData).save()
+  .then(function(data){
+    res.status(200).json({ error: false, data: { message: 'imagen creado' } });
+  })
+  .catch(function (err) {
+    res.status(500).json({ error: true, data: {message: err.message} });
+  });
+
+}
+
+exports.findOneDocument = (req,res) => {
+
+  let conditions = { id: req.params.id };
+
+  Imagen.forge(conditions).fetch()
+    .then(function(data){
+      if(!data) return res.status(404).json({ error : true, data : { message : 'imagen no existe' } });
+
+      res.status(200).json({ error : false, data : data.toJSON() })
+
+    })
+    .catch(function(err){
+      res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
+
+exports.updateDocument = (req,res) => {
+
+  let conditions = { id: req.params.id };
+
+  Imagen.forge(conditions).fetch()
+    .then(function(imagen){
+      if(!imagen) return res.status(404).json({ error : true, data : { message : 'imagen no existe' } });
+
+      let updateData = {
+        id_sistema:         req.body.id_sistema,
+        imagen:             req.body.imagen,
+        titulo:             req.body.cedula,
+        descripcion:        req.body.descripcion,
+        fecha_creacion:     req.body.fecha_creacion,
+        estatus:            req.body.estatus,
+        tipo_imagen:        req.body.tipo_imagen,
+      }
+      
+      imagen.save(updateData)
+        .then(function(data){
+          res.status(200).json({ error : false, data : { message : 'imagen actualizado'} });
+        })
+        .catch(function(err){
+          res.status(500).json({ error : false, data : {message : err.message} });
+        })
+
+    })
+    .catch(function(err){
+          res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
+
+exports.deleteDocument = (req,res) => {
+
+  let conditions = { id: req.params.id };
+
+  Imagen.forge(conditions).fetch()
+    .then(function(imagen){
+      if(!imagen) return res.status(404).json({ error : true, data : { message : 'imagen no existe' } });
+
+      imagen.destroy()
+        .then(function(data){
+          res.status(200).json({ error : false, data : {message : 'imagen eliminado'} })
+        })
+        .catch(function(err){
+          res.status(500).json({error : true, data : {message : err.message}});
+        })
+
+    })
+    .catch(function(err){
+          res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
---
+++ controllers/servicio.js
@@ -0,0 +1,115 @@
+//----dependencias------  
+'use strict'
+const bcrypt = require("bcryptjs");
+const Servicio = require('../models/servicio');
+
+exports.findDocuments = (req,res) => {
+  
+  Servicio.forge().fetchAll()
+  .then(function(data){
+    res.status(200).json({ error : false, data : data.toJSON() });
+  })
+  .catch(function (err) {
+    res.status(500).json({ error: true, data: {message: err.message} });
+  });
+
+}
+
+exports.createDocument = (req,res) => {
+
+  let newData = {
+    imagen:             req.body.imagen,
+    id_tipo_servicio:   req.body.id_tipo_servicio,
+    nombre:             req.body.nombre,
+    precio:             req.body.precio,
+    descripcion:        req.body.descripcion,
+    duracion:           req.body.duracion,
+    estatus:            req.body.estatus,
+    fecha_creacion:     req.body.fecha_creacion,
+    visible:            req.body.visible,
+  }
+
+  Servicio.forge(newData).save()
+  .then(function(data){
+    res.status(200).json({ error: false, data: { message: 'servicio creado' } });
+  })
+  .catch(function (err) {
+    res.status(500).json({ error: true, data: {message: err.message} });
+  });
+
+}
+
+exports.findOneDocument = (req,res) => {
+
+  let conditions = { id: req.params.id };
+
+  Servicio.forge(conditions).fetch()
+    .then(function(data){
+      if(!data) return res.status(404).json({ error : true, data : { message : 'servicio no existe' } });
+
+      res.status(200).json({ error : false, data : data.toJSON() })
+
+    })
+    .catch(function(err){
+      res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
+
+exports.updateDocument = (req,res) => {
+
+  let conditions = { id: req.params.id };
+
+  Servicio.forge(conditions).fetch()
+    .then(function(servicio){
+      if(!servicio) return res.status(404).json({ error : true, data : { message : 'servicio no existe' } });
+
+      let updateData = {
+        imagen:             req.body.imagen,
+        id_tipo_servicio:   req.body.id_tipo_servicio,
+        nombre:             req.body.nombre,
+        precio:             req.body.precio,
+        descripcion:        req.body.descripcion,
+        duracion:           req.body.duracion,
+        estatus:            req.body.estatus,
+        fecha_creacion:     req.body.fecha_creacion,
+        visible:            req.body.visible,
+      } 
+      
+      servicio.save(updateData)
+        .then(function(data){
+          res.status(200).json({ error : false, data : { message : 'servicio actualizado'} });
+        })
+        .catch(function(err){
+          res.status(500).json({ error : false, data : {message : err.message} });
+        })
+
+    })
+    .catch(function(err){
+          res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
+
+exports.deleteDocument = (req,res) => {
+
+  let conditions = { id: req.params.id };
+
+  Servicio.forge(conditions).fetch()
+    .then(function(servicio){
+      if(!servicio) return res.status(404).json({ error : true, data : { message : 'servicio no existe' } });
+
+      servicio.destroy()
+        .then(function(data){
+          res.status(200).json({ error : false, data : {message : 'servicio eliminado'} })
+        })
+        .catch(function(err){
+          res.status(500).json({error : true, data : {message : err.message}});
+        })
+
+    })
+    .catch(function(err){
+          res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
