308f73953567969959379ecf89892e6283c36ad9
digital-domain <sohecdyavila38@gmail.com>
2018-05-09 23:15:57 -0400

    servicio de cliente con sus solicitudes

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
       port     : '5432', //5432
       user     : 'postgres',
       password : 'postgres',
-      database : 'mystique',
+      database : 'devmystique',
       charset  : 'utf8'
     }
   }
