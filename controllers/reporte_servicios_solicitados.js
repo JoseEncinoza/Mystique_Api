b00109a271055c1fc745a39934af872fc815b1db
Jose Encinoza <joseEncinoza07@gmail.com>
2018-06-03 01:52:29 -0400

    agregar servicio

--- controllers/reporte_servicios_solicitados.js
+++ controllers/reporte_servicios_solicitados.js
@@ -5,7 +5,7 @@ const Reporte_servicios_solicitados = require('../models/reporte_servicios_solic
 
 exports.reporte = (req,res) => {
 
-  //http://localhost:3000/api/reporte_servicios_solicitados?fecha_inicio=01-02-2018&fecha_fin=02-02-2018
+  //http://localhost:3000/api/reporte_servicios_solicitados?fecha_inicio=01-02-2018&fecha_fin=02-02-2018&orderby=ASC
 
   //parametro fechas
   if(req.query.fecha_inicio){
@@ -25,6 +25,7 @@ exports.reporte = (req,res) => {
     qb.column('id_servicio','nombre_servicio','tipo_servicio');
     qb.count("id_servicio");
     qb.groupBy('id_servicio','nombre_servicio','tipo_servicio');
+    qb.orderBy('count', req.query.orderby);
     qb.whereBetween('fecha_creacion', [fecha_inicio, fecha_fin+' 23:59']);
   }).fetchAll({})
   .then(function(data){
