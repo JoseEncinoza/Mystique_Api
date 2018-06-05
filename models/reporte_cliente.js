cc423ec1abf8fd81e780fdb0aecb4ab8162ab020
Jose Encinoza <joseEncinoza07@gmail.com>
2018-06-04 22:13:13 -0400

    agregar servicio

--- models/reporte_cliente.js
+++ models/reporte_cliente.js
@@ -3,7 +3,7 @@ const Bookshelf = require('../db');
 const Vista_perfil = require('./vista_perfil');
 
 const Reporte_servicio = Bookshelf.Model.extend({
-  tableName: 'cliente',
+  tableName: 'vista_cliente_ciudad',
   perfil: function(){
   	return this.hasMany(Vista_perfil,"id_cliente")
   }
